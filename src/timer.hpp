04323366676ef58b6a6429e3617240efed2263ae
klb6542 <keeganthebates2023@gmail.com>
2025-02-03 19:35:57 -0500

    Changed alot with this class. Mainly just have one method to set a timer for x amount of seconds and then it has a callback function that is called at the end of it. Learned alot about lvalues and rvalues.

--- src/timer.hpp
+++ src/timer.hpp
@@ -1,29 +1,18 @@
 #pragma once
 #include <raylib.h>
-#include <stdexcept>
 #include <thread>
+#include <utility>
 
 class Timer {
     public:
-        Timer(double seconds);
-
         template<typename Func, typename... Args>
-        void WhenDone(const Func& func, Args&... args);
-    private:
-        double m_endTime;
-        bool m_initiated{};
+        void Start(double seconds, const Func& func, Args&&... args);
 };
 
 template <typename Func, typename... Args>
-void Timer::WhenDone(const Func& func, Args&... args) {
-    if (this->m_initiated)
-        throw std::runtime_error("You may only call this method once!");
-
-    this->m_initiated = true;
-    this->m_endTime += GetTime();
-
-    std::thread([func, args..., this]() {
-        std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<int>(this->m_endTime * 1000)));
-        func(args...);
+void Timer::Start(double seconds, const Func& func, Args&&... args) {
+    std::thread([seconds, func, args..., this]() {
+        std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<int>(seconds * 1000)));
+        func(std::forward<Args>(args)...);
     }).detach();
 }
